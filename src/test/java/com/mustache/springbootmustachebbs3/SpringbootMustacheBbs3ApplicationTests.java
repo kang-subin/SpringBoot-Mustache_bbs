8fb3bdf1795cb1194e1dc5a2a11bbead56a557b9
강수빈 <tnbin98@gmail.com>
2022-11-09 18:22:10 +0900

    first commit

---
+++ src/test/java/com/mustache/springbootmustachebbs3/SpringbootMustacheBbs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.springbootmustachebbs3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustacheBbs3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
